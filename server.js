7075893ec7fbe6f0a7fb0c2404d83462c33973c6
edend85 <117260021+edend85@users.noreply.github.com>
2024-01-11 16:40:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const DB = require('./utils/db');
 const PORT = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 
-
-server.use(cors({
+/*{
     origin:["http://localhost:5173","https://authentication_website.ordernet.com"]
-}));
+}*/ 
+server.use(cors());
 server.use(express.json());
 
 server.use('/api/user', require('./routes/userRoute'));
