fb131f5bc4cabf6d5554635316466c3b099c7fc1
edend85 <117260021+edend85@users.noreply.github.com>
2024-01-16 10:39:33 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const DB = require('./utils/db');
 const PORT = process.env.PORT || 3000;
 const db = new DB();
 
-server.use(cors({origin:["https://authentication-zi9e.onrender.com","https://authentication_website.ordernet.com","https://www.googleapis.com/oauth2"]}));
+server.use(cors({origin:["https://authentication-zi9e.onrender.com","https://authentication_website.ordernet.com","https://lh3.googleusercontent.com/a-/ALV-UjXJBu6TS…D8xJSl8xHTnRdDbuRr_NYtBygCq3yS_WR9ozqnFSjLN=s96-c"]}));
 server.use(express.json({ limit: '100mb'}));
 
 server.use('/api/user', require('./routes/userRoute'));
