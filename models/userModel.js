795fef639c12c13800f18477d577ce51a9ef3a8b
edend85 <117260021+edend85@users.noreply.github.com>
2024-03-03 15:19:16 +0200

    adding Check func

--- models/userModel.js
+++ models/userModel.js
@@ -1,43 +1,49 @@
 const DB = require('../utils/db');
 const bcrypt = require('bcrypt');
-class User{
+class User {
     firstName;
     lastName;
     email;
     password;
     googleAccount;
 
-    constructor(firstName,lastName,email,password,googleAccount)
-    {
+    constructor(firstName, lastName, email, password, googleAccount) {
         this.firstName = firstName,
-        this.lastName = lastName,
-        this.email = email,
-        this.password = password,
-        this.googleAccount = googleAccount
+            this.lastName = lastName,
+            this.email = email,
+            this.password = password,
+            this.googleAccount = googleAccount
 
     }
-    static async InsertUser(firstName,lastName,email,password,socialMediaAccount){
+    static async InsertUser(firstName, lastName, email, password, socialMediaAccount) {
         console.log('step modal InsertUser :>> ');
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
-        this.password = await bcrypt.hash(password,10);
+        this.password = await bcrypt.hash(password, 10);
         this.socialMediaAccount = socialMediaAccount;
-         return await new DB().InsertUser('Users',{...this});
-     }
-     static async Login(email,password){
+        return await new DB().InsertUser('Users', {
+            ...this
+        });
+    }
+    static async Login(email, password) {
         console.log('step 2:>> ');
         this.email = email;
-        let user = await new DB().Login('Users',email);
+        let user = await new DB().Login('Users', email);
         console.log('back to step 2 :>> ');
-        let passwordMatch = await bcrypt.compare(password,user.password);
-        if(passwordMatch){
+        let passwordMatch = await bcrypt.compare(password, user.password);
+        if (passwordMatch) {
             return user;
-        }
-        else{
+        } else {
             return false;
         }
-     }
+    }
+    static async Check(email) {
+        console.log('step 2:>> ');
+        this.email = email;
+        console.log('back to step 2 :>> ');
+        return userEmail = await new DB().Check('Users', email);
+    }
 }
 
 module.exports = User;
